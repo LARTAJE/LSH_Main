c432c1f7e12131f43ebbe4ec32125c1c91904256
Lunny Nymphet <99461156+LARTAJE@users.noreply.github.com>
2025-05-25 22:48:02 -0300

    Update LSH_Loader.lua
    Fixed Againnn to the new repo XD

--- LSH_Loader.lua
+++ LSH_Loader.lua
@@ -1,4 +1,4 @@
 local PlayerService = game:GetService("Players")
 local GameId = game.GameId
-local Repository = "https://raw.githubusercontent.com/LARTAJE/LSH_Main/main/Game_".. GameId ..".lua"
+local Repository = "https://raw.githubusercontent.com/LARTAJE/LSH_V2_Main/refs/heads/main/Game_".. GameId ..".lua"
 loadstring(game:HttpGet(Repository))()
